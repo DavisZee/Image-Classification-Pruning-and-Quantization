d6dda4b108b4e4a46aaf8f12a8d628a71ad9682a
LucadeRaad <lucaderaad@outlook.com>
2022-06-01 23:03:25 -0700

    First Commit!
    added the two .py files

---
+++ Benchmark/main.py
@@ -0,0 +1,123 @@
+# CSS 490 Final Project Implemntation Part 3
+# Names: Luca de Raad and Davis Zhong
+# Date: 05/29/2022
+# Description: This python file creates and evaluates four pretrained InceptionNetV3 
+# models using the Keras mnist dataset. Models include normal unmodified InceptionV3,
+# pruned model of InceptionNetV3, quantized model of InceptionNetV3, and both pruned
+# and quantized model of InceptionNetV3.
+
+
+# imports
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+from time import time
+from pathlib import Path
+
+# load in dataset
+mnist = keras.datasets.mnist
+(test_images, test_labels) = mnist.load_data()[1]
+
+# image count to control the amount of images tested
+max_images = 1000
+
+test_images = test_images[0:max_images, 0:28, 0:28]
+
+test_images = np.expand_dims(test_images, axis=3)
+test_images = tf.image.resize(test_images, [75, 75])
+test_images = test_images / 255
+
+
+# runs inferencing on a model and calculates relevant metrics
+def evaluate_model(evaluate_interpreter, model_name):
+    input_index = evaluate_interpreter.get_input_details()[0]["index"]
+    output_index = evaluate_interpreter.get_output_details()[0]["index"]
+
+    # metric variables
+    total_time_interpreting = 0
+    slowest_inference_time = 0
+    fastest_inference_time = 99999
+
+    prediction_digits = []
+
+    # image inferencing loop
+    for test_image in test_images:
+        # Pre-processing: add batch dimension and convert to float32 to match with
+        # the model's input data format.
+        test_image = np.expand_dims(test_image, axis=0).astype(np.float32)
+        evaluate_interpreter.set_tensor(input_index, test_image)
+
+        start = time()
+
+        # Run inference.
+        evaluate_interpreter.invoke()
+
+        end = time()
+
+        # calculating metrics
+        total_time_interpreting += end - start
+        fastest_inference_time = min(fastest_inference_time, end - start)
+        slowest_inference_time = max(slowest_inference_time, end - start)
+
+        # Post-processing: remove batch dimension and find the digit with highest
+        # probability.
+        output = evaluate_interpreter.tensor(output_index)
+        digit = np.argmax(output()[0])
+        prediction_digits.append(digit)
+
+    print('\n')
+    # Compare prediction results with ground truth labels to calculate accuracy.
+    prediction_digits = np.array(prediction_digits)
+
+    num_correct = 0
+
+    for i in range(max_images):
+        if prediction_digits[i] == test_labels[i]:
+            num_correct += 1
+
+    print(f"Model {model_name} was trained on {max_images}")
+
+    accuracy = num_correct / max_images
+    print(f"Top 1 accuracy was {accuracy}")
+
+    print(f"Total time spent was {total_time_interpreting}")
+
+    avg_inference_time = total_time_interpreting / max_images
+    print(f"Average inference time for each image was {avg_inference_time}")
+
+    print(f"Fastest inference time for an image was {fastest_inference_time}")
+    print(f"Slowest inference time for an image was {slowest_inference_time}")
+
+
+# Interprets tflite models obtained using a converter
+def benchmark_tflite(model_name):
+    tflite_models_dir = Path.cwd()
+
+    model = tflite_models_dir / model_name
+    interpreter = tf.lite.Interpreter(model_path=str(model))
+    interpreter.allocate_tensors()
+
+    print("Beginning evaluation of ", model_name)
+
+    evaluate_model(interpreter, model_name)
+
+    print("Evaluation of ", model_name, " complete!")
+
+    del interpreter
+
+
+# main driver method
+def main():
+    benchmark_tflite("normal.tflite")
+
+    benchmark_tflite("pruned.tflite")
+
+    benchmark_tflite("quantized.tflite")
+
+    benchmark_tflite("pruned_quantized.tflite")
+
+    print("done!")
+
+
+if __name__ == '__main__':
+    main()
---
+++ QuantizeAndPrune/main.py
@@ -0,0 +1,113 @@
+import tempfile
+from pathlib import Path
+
+from tensorflow.python.ops.numpy_ops import np_config
+import tensorflow.keras as keras
+import tensorflow as tf
+# from tensorflow import keras
+import numpy as np
+import tensorflow_model_optimization as tfmot
+
+tflite_models_dir = Path.cwd()
+
+input_shape = (75, 75, 1)
+
+
+def main():
+    np_config.enable_numpy_behavior()
+    # Load MNIST dataset
+    mnist = keras.datasets.mnist
+    (train_images, train_labels) = mnist.load_data()[0]
+
+    # Normalize the input image so that each pixel value is between 0 and 1.
+    train_images = train_images / 255.0
+
+    train_images = np.expand_dims(train_images, axis=3)
+    train_images = tf.image.resize(train_images, [75, 75])
+
+
+    # Define the model architecture.
+    model = tf.keras.applications.InceptionV3(weights=None, input_shape=input_shape, classes=10)
+
+    # Train the digit classification model
+    model.compile(optimizer='adam',
+                  loss=
+                  tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+
+    model.fit(
+        train_images,
+        train_labels,
+        epochs=4,
+        validation_split=0.1,
+    )
+
+    prune_low_magnitude = tfmot.sparsity.keras.prune_low_magnitude
+
+    # Compute end step to finish pruning after 2 epochs.
+    batch_size = 128
+    epochs = 2
+    validation_split = 0.1  # 10% of training set will be used for validation set.
+
+    num_images = train_images.shape[0] * (1 - validation_split)
+    end_step = np.ceil(num_images / batch_size).astype(np.int32) * epochs
+
+    # Define model for pruning.
+    pruning_params = {
+        'pruning_schedule': tfmot.sparsity.keras.PolynomialDecay(initial_sparsity=0.50,
+                                                                 final_sparsity=0.80,
+                                                                 begin_step=0,
+                                                                 end_step=end_step)
+    }
+
+    model_for_pruning = prune_low_magnitude(model, **pruning_params)
+
+    # `prune_low_magnitude` requires a recompile.
+    model_for_pruning.compile(optimizer='adam',
+                              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                              metrics=['accuracy'])
+
+    # model_for_pruning.summary()
+
+    logdir = tempfile.mkdtemp()
+
+    callbacks = [
+        tfmot.sparsity.keras.UpdatePruningStep(),
+        tfmot.sparsity.keras.PruningSummaries(log_dir=logdir),
+    ]
+
+    model_for_pruning.fit(train_images, train_labels,
+                          batch_size=batch_size, epochs=epochs, validation_split=validation_split,
+                          callbacks=callbacks)
+
+    converter_base = tf.lite.TFLiteConverter.from_keras_model(model)
+    normal_tflite_model = converter_base.convert()
+
+    save_model(normal_tflite_model, 'normal.tflite', False)
+
+    save_model(normal_tflite_model, 'quantized.tflite', True)
+
+    model_for_export = tfmot.sparsity.keras.strip_pruning(model_for_pruning)
+
+    converter = tf.lite.TFLiteConverter.from_keras_model(model_for_export)
+    pruned_tflite_model = converter.convert()
+
+    save_model(pruned_tflite_model, 'pruned.tflite', False)
+
+    save_model(pruned_tflite_model, 'pruned_quantized.tflite', True)
+
+
+def save_model(model, model_name, quantize):
+    tflite_file = tflite_models_dir / model_name
+
+    if quantize:
+        model.converter.optimizations = [tf.lite.Optimize.DEFAULT]
+
+    with open(tflite_file, 'wb') as f:
+        f.write(model)
+
+    print(f'Saved {model_name} TFLite model to:', tflite_file)
+
+
+if __name__ == '__main__':
+    main()
